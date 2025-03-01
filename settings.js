5e459a0314af943b9b78cf24c37617c0383e761b
Mehar-sab527 <aligee7772@gmail.com>
2025-03-01 23:15:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "mehar_sab",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '923149264228', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
